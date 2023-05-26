fbfda133e616381809409200f221ff2bfe408b7d
MahdiRafsan <marjanchowdhury92@gmail.com>
2023-05-26 12:57:17 -0400

    Added redirects file for netlify

--- backend/api/config/constants.js
+++ backend/api/config/constants.js
@@ -1,15 +1,13 @@
 const CORS_OPTIONS = {
-  origin: ["https://job-trek.onrender.com"],
+  origin: ["https://job-trek.netlify.app"],
   credentials: true,
 };
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "Strict",
+  sameSite: "strict",
   maxAge: 15 * 24 * 60 * 60 * 1000, // expires after 15 days
-  domain: "job-trek.onrender.com",
-  path: "/"
 };
 
 module.exports = { CORS_OPTIONS, COOKIE_OPTIONS };
