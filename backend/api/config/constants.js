c5f282dbedfe942c507b5b87f41e280793f5548e
MahdiRafsan <marjanchowdhury92@gmail.com>
2023-05-26 14:38:48 -0400

    Added domain for cookie

--- backend/api/config/constants.js
+++ backend/api/config/constants.js
@@ -1,13 +1,14 @@
 const CORS_OPTIONS = {
-  origin: ["https://job-trek.netlify.app"],
+  origin: ["https://job-trek.onrender.com"],
   credentials: true,
 };
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "None",
+  sameSite: "strict",
   maxAge: 15 * 24 * 60 * 60 * 1000, // expires after 15 days
+  domain: ".onrender.com",
 };
 
 module.exports = { CORS_OPTIONS, COOKIE_OPTIONS };
